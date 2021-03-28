c46f76613a8c8f630c0d18d9403a4a041b496f16
Alexei <mralexxxx2012@gmail.com>
2021-03-28 18:24:36 +0300

    fixed homework 1-2

--- app/src/main/java/com/example/myprojectforandroidacademy/FragmentMoviesList.kt
+++ app/src/main/java/com/example/myprojectforandroidacademy/FragmentMoviesList.kt
@@ -5,20 +5,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import com.example.myprojectforandroidacademy.databinding.FragmentMoviesListBinding
 
 class FragmentMoviesList : Fragment() {
 
-    private var moviesListClickListener: MoviesListClickListener? = null
-    private lateinit var fragmentBinding: FragmentMoviesListBinding
+    private var mMoviesListClickListener: MoviesListClickListener? = null
+    private var mFragmentBinding: FragmentMoviesListBinding? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        fragmentBinding.cardView?.apply {
+        mFragmentBinding?.containerMovieItem?.root?.apply {
             setOnClickListener {
-                moviesListClickListener?.openMovieDetails()
+                mMoviesListClickListener?.openMovieDetails()
             }
         }
     }
@@ -26,13 +25,14 @@ class FragmentMoviesList : Fragment() {
     override fun onAttach(context: Context) {
         super.onAttach(context)
         if (context is MoviesListClickListener) {
-            moviesListClickListener = context
+            mMoviesListClickListener = context
         }
     }
 
     override fun onDetach() {
         super.onDetach()
-        moviesListClickListener = null
+        mMoviesListClickListener = null
+        mFragmentBinding = null
     }
 
     override fun onCreateView(
@@ -40,8 +40,8 @@ class FragmentMoviesList : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        fragmentBinding = FragmentMoviesListBinding.inflate(inflater, container, false)
-        return fragmentBinding?.root
+        mFragmentBinding = FragmentMoviesListBinding.inflate(inflater, container, false)
+        return mFragmentBinding?.root
     }
 
     interface MoviesListClickListener {
